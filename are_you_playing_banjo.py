202d8eee669468c27a8a7f138c0aae3c3552fe6b
BogdanYatsiv <bogdanyatsiv@gmail.com>
2019-08-12 10:51:37 +0300

    Delete are_you_playing_banjo.py

--- are_you_playing_banjo.py
+++
@@ -1,6 +0,0 @@
-def areYouPlayingBanjo(name):
-    temp = list(name)
-    if temp[0] == 'r' or temp[0] == 'R':
-        return name+' plays banjo'
-    else:
-        return name+' does not play banjo'
