5c5140854fc1faaba636ba1d7d5a459e07507965
BogdanYatsiv <bogdanyatsiv@gmail.com>
2019-08-20 23:46:51 +0300

    update

--- 19.08_task_2.py
+++ 19.08_task_2.py
@@ -1,4 +1,10 @@
 from math import pow , pi
 
-print("Square of rectangle")
-a = float(input("Enter a side: "))
+def get_rec_sqr(a:float,b:float):
+    print(f"Square = {a*b}")
+
+def get_triang_sqr(h:float,a:float):
+    print(f"Square = {0.5*h*a}")
+
+def get_cir_sqr(r:float):
+    print(f"Square = {pi*pow(r,2)}")
